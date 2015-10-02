692b7584a00fcc7c69dfd60079990e1a940a05a1
Tobias Bayer <tobayer@googlemail.com>
2015-10-02 15:42:20 +0200

    Parse json request entity by default

--- src/liberator/parse.clj
+++ src/liberator/parse.clj
@@ -1,11 +1,12 @@
 (ns liberator.parse
   (:require [clojure.data.json :as json]))
 
-(defmulti parse-request-entity (fn [ctx] (get-in ctx [:headers "content-type"])))
+(defmulti parse-request-entity
+  (fn [ctx] (get-in ctx [:request :headers "content-type"])))
 
 (defmethod parse-request-entity "application/json" [ctx]
   (if-let [body (:body (:request ctx))]
-    {:request-entity (json/read-str body)}
+    {:request-entity (json/read-str (slurp body) :key-fn keyword)}
     true))
 
 (defmethod parse-request-entity :default [ctx]
--- test/test_parse.clj
+++ test/test_parse.clj
@@ -1,16 +1,19 @@
 (ns test-parse
   (:use liberator.core
-        [liberator.representation :only (ring-response)]
         midje.sweet
         checkers
-        [ring.mock.request :only [request header]]
+        [ring.mock.request :only [request]]
         [clojure.data.json :only [write-str]]))
+
+(def request-entity (atom nil))
 
 (facts "about entity parsing"
   (fact "it parses a json entity"
     (let [r (resource :allowed-methods [:post]
-                      :handle-created (fn [ctx] (println ctx)))
+                      :handle-created (fn [ctx] (reset! request-entity (:request-entity ctx)) "created"))
           resp (r (-> (request :post "/" )
                       (ring.mock.request/body (write-str {:foo "bar"}))
                       (ring.mock.request/content-type "application/json")))]
-      resp => (is-status 201))))
+      resp => (is-status 201)
+      @request-entity => {:foo "bar"}))
+  (against-background (before :facts (reset! request-entity nil))))
